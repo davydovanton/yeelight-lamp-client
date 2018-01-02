ebc22c83e18791f22ed14a82fd54821d1bb67034
Anton Davydov <antondavydov.o@gmail.com>
2018-01-02 15:08:06 +0300

    Initialize project

---
+++ lib/yeelight.rb
@@ -0,0 +1,5 @@
+require "yeelight/version"
+
+module Yeelight
+  # Your code goes here...
+end
---
+++ test/yeelight/lamp/client_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class Yeelight::Lamp::ClientTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Yeelight::Lamp::Client::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
